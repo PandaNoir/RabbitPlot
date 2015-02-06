6fe5e74410e32a5d962ed123448b752682b2ccde
PandaNoir <moon_in_france@yahoo.co.jp>
2015-02-06 22:41:07 +0900

    Revert "Merge branch 'directive'"
    This reverts commit 7ace1a2bcb83d8a24c8974401546600d9528a225, reversing
    changes made to 6663d209c9e1120af63812b00d5782c8e0db481a.

--- js/detailCtrl.js
+++
@@ -1,6 +0,0 @@
-angular.module(appName)
-.controller('detailCtrl',['$scope','eventCal','calF','mode',function($scope,eventCal,calF,mode){
-    $scope.mode=mode;
-    $scope.calF=calF;
-    $scope.eventCalendar=eventCal.eventCalendar;
-}]);
