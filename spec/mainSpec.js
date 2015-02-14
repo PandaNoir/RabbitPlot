2e4f75ea7c031bfa2d03988ab83ed850dd7db9ea
PandaNoir <moon_in_france@yahoo.co.jp>
2015-02-14 16:12:09 +0900

    calF.calendar()のテストを追加
    テストの名前がおかしくなったけど英語ができないのでだれかなんとかしてください

--- spec/mainSpec.js
+++ spec/mainSpec.js
@@ -62,4 +62,17 @@ describe('test',function(){
             expect(eventCal.splitSelector('(key:value and key:value) and key:value')).toEqual([['(',LPARENTHESES],['key:value',OTHERS],['&&',OPERATOR],['key:value',OTHERS],[')',RPARENTHESES],['&&',OPERATOR],['key:value',OTHERS]]);
         }));
     });
+    describe('calendar.calendar()',function(){
+        var OVER_MONTH=64;
+
+        it('should be real calendar.',inject(function(calF){
+            var c=calF.calendar(2014,2-1);
+            var exp=[[0,0,0,0,0,0,1],[2,3,4,5,6,7,8],[9,10,11,12,13,14,15],[16,17,18,19,20,21,22],[23,24,25,26,27,28,OVER_MONTH]];
+            exp.year=2014;
+            exp.month=1;
+            expect(c).toEqual(exp);
+            expect(JSON.stringify(calF.calendar(2014,2-1))).toEqual(JSON.stringify([[0,0,0,0,0,0,1],[2,3,4,5,6,7,8],[9,10,11,12,13,14,15],[16,17,18,19,20,21,22],[23,24,25,26,27,28,OVER_MONTH]]));
+            expect(_.flatten(calF.calendar(2014,2-1))).toEqual([0,0,0,0,0,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,OVER_MONTH]);
+        }));
+    });
 });
