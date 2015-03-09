29eb34b7edc5434c4dd9cf5df8bbd0b1121fea78
PandaNoir <moon_in_france@yahoo.co.jp>
2015-03-09 13:30:10 +0900

    登録画面作成完了
    登録できるようになった。

--- js/signup.js
+++ js/signup.js
@@ -1,10 +1,9 @@
 $(function(){
-    var database='http://www40.atpages.jp/chatblanc/genderC/database.php';
-    database='http://localhost/hoge.php';
+    var database='http://www40.atpages.jp/chatblanc/genderC/';
     $('#confirm').on('click',function(e){
         e.stopPropagation();
         $('#waiting').show();
-        $.post(database,{'username':$('#username').val()},function(mes){
+        $.post(database+'confirm.php',{'username':$('#username').val()},function(mes){
             console.log(mes);
             if(mes==='OK'){
                 $('#canUse').text('使えます');
@@ -15,13 +14,26 @@ $(function(){
         });
     });
     $('#signup').on('click',function(e){
-        $.post(database,{'username':$('#username').val(),'password':(new jsSHA($('#password').val(),'TEXT')).getHash('SHA-384','HEX')},function(mes){
+        $.post(database+'signup.php',{'username':$('#username').val(),'password':(new jsSHA($('#password').val(),'TEXT')).getHash('SHA-384','HEX'),'id':uuid()},function(mes){
             console.log(mes);
             if(mes==='OK'){
                 alert('登録完了しました.');
+                location.href='http://pandanoir.web.fc2.com/RabbitPlot';
             }else if(mes==='USERNAME'){
                 alert('すでに'+$('#username').val()+'というユーザー名は使用されています.');
             }
         });
     });
+    function uuid() {
+        var uuid = "", i, random;
+        for (i = 0; i < 32; i++) {
+            random = Math.random() * 16 | 0;
+
+            if (i === 8 || i === 12 || i === 16 || i === 20) {
+                uuid += "-"
+            }
+            uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random)).toString(16);
+        }
+        return uuid;
+    };
 });
